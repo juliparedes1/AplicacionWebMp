c8ea09d0c6d17969b455d0b84062ad2e4de61861
juliCoder <juliparedes451@gmail.com>
2024-08-29 17:05:04 -0300

    Creacion de mi primer SP y agredado de lo necesario para comenzar la practica

---
+++ WebApplication1/ListaPokemons.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Negocio;
+
+namespace WebApplication1
+{
+    public partial class ListaPokemons : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            PokemonNegocio n1 = new PokemonNegocio();
+            dgvPokemons.DataSource = n1.listarConSp();
+            dgvPokemons.DataBind();
+            
+        }
+    }
+}
